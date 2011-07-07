35746a92b43c73b205cbe0e14cf2a455c4d3a378
David Zabinski <dzabinski2011@kellogg.northwestern.edu>
2011-07-06 20:30:42 -0500

    Cleaned up admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -14,42 +14,4 @@ class AdminController < ApplicationController
     
     @orders = @orders.values
   end
-
-  def send_boxes_search
-
-  end
-
-  # TODO: I don't think this is used anymore. Cut?
-  def send_boxes_user_search
-    conditions = Array.new 
-    condition_count = 0
-    condition_string = ''
-
-    if (!params[:user][:id].empty?)
-      condition_count = condition_count + 1
-      condition_string = 'ID = ?'
-      conditions[condition_count] = params[:user][:id]
-    end
-
-    if (!params[:user][:first_name].empty?)
-      if (condition_count > 0)
-        condition_string << " OR "
-      end
-      condition_count = condition_count + 1
-      condition_string << 'first_name LIKE ?'
-      conditions[condition_count] = params[:user][:first_name]
-    end
-
-    if (!params[:user][:last_name].empty?)
-      if (condition_count > 0)
-        condition_string << " OR "
-      end
-      condition_count = condition_count + 1
-      condition_string << 'last_name LIKE ?'
-      conditions[condition_count] = params[:user][:last_name]
-    end
-
-    conditions[0] = condition_string
-    @found_users = User.all(:conditions => conditions, :order => 'last_name ASC')
-  end
 end
