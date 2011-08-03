7264a473e3e415f3dc9fdd620794e9fa2ef66aef
David Zabinski <dzabinski2011@kellogg.northwestern.edu>
2011-08-03 18:19:15 -0500

    Fixed an error that Lorena found, and made other areas more robust

--- 20110803204508_add_next_box_num_to_customer.rb
+++
@@ -1,9 +0,0 @@
-class AddNextBoxNumToCustomer < ActiveRecord::Migration
-  def self.up
-    add_column :users, :last_box_num, :integer
-  end
-
-  def self.down
-    remove_column :users, :last_box_num
-  end
-end
