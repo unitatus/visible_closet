e302637684bd1844cbc0090c5ebf41461cf6225a
David Zabinski <dzabinski2011@kellogg.northwestern.edu>
2011-09-30 22:09:43 -0500

    Another fix for sending emails with the test user concept

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -8,7 +8,7 @@ class AdminMailer < ActionMailer::Base
     mail(:subject => "New Interested Person")
   end
   
-  def deliver_new_order(user, order, invoice, vc_address, payment_profile, billing_address)
+  def AdminMailer.deliver_new_order(user, order, invoice, vc_address, payment_profile, billing_address)
     if user.not_test_user?
       new_order(user, order, invoice, vc_address, payment_profile, billing_address).deliver
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   helper :application
   layout 'user_mailer', :except => :invoice_email
   
-  def deliver_invoice_email(user, invoice, send_admin=false)
+  def UserMailer.deliver_invoice_email(user, invoice, send_admin=false)
     if user.not_test_user?
       invoice_email(user, invoice, send_admin).deliver
     end
@@ -32,7 +32,7 @@ class UserMailer < ActionMailer::Base
     end
   end
   
-  def deliver_boxes_sent(user, order_lines)
+  def UserMailer.deliver_boxes_sent(user, order_lines)
     if user.not_test_user?
       boxes_sent(user, order_lines).deliver
     end
@@ -44,7 +44,7 @@ class UserMailer < ActionMailer::Base
     mail(:to => user.email, :subject => "Notification from The Visible Closet")
   end
   
-  def deliver_box_received(box)
+  def UserMailer.deliver_box_received(box)
     if box.user.not_test_user?
       box_received(box).deliver
     end
@@ -55,7 +55,7 @@ class UserMailer < ActionMailer::Base
     mail(:to => box.user.email, :subject => "Notification from The Visible Closet")
   end
   
-  def deliver_box_inventoried(box)
+  def UserMailer.deliver_box_inventoried(box)
     if box.user.not_test_user?
       box_inventoried(box).deliver
     end
