a1f5a9fc0e847715eecad88905a9d31bb4c6d72a
David Zabinski <dave@thevisiblecloset.com>
2011-11-06 17:27:46 -0600

    Tweaks to donation menu to make it work again on all browsers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -139,16 +139,16 @@ $(document).ready(function(){
           }
     );
 
-    $('.browse-item-menu').hover(
-          function () {
-            $($(this)).find("div").toggle();
-          }, 
-          function () {
-            $($(this)).find("div").toggle();
-          }
-    );
+    // $('.browse-item-menu').hover(
+    //       function () {
+    //         $($(this)).find("div").toggle();
+    //       }, 
+    //       function () {
+    //         $($(this)).find("div").toggle();
+    //       }
+    // );
     
-    $('.browse-item-menu').click(
+    $('.browse-item-menu-link').click(
           function () {
 	alert('well fuck')
             $($(this)).find("div").toggle();
