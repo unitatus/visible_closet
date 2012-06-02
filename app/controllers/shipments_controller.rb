e28711a1cc488fb425509a744794cfdcc59be4e8
David Zabinski <dave@thevisiblecloset.com>
2012-06-02 18:51:34 -0500

    Attempt to fix get_label for returned shipments

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -4,6 +4,10 @@ class ShipmentsController < ApplicationController
   def get_label
     shipment = Shipment.find(params[:id])
 
+    if !shipment.has_shipment_label?
+      shipment.generate_fedex_label
+    end
+
     send_data(shipment.shipment_label, :filename => shipment.shipment_label_file_name_short, :type => "application/pdf")
   end
 end
